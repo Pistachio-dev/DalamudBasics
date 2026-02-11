fbbfd8ba7184257c7a70303f1b6bae8ea3861bdb
Pistachio-dev <177745391+Pistachio-dev@users.noreply.github.com>
2026-02-11 11:43:37 +0100

    Added delay on dice command

--- DalamudBasics/Chat/Output/ChatOutput.cs
+++ DalamudBasics/Chat/Output/ChatOutput.cs
@@ -70,8 +70,8 @@ namespace DalamudBasics.Chat.Output
         {
             // Put the chat into /party or /alliance mode
             var firstCommand = isAllianceChat ? "/a" : "/p";
-            WriteCommand($"{firstCommand}");
-            WriteCommand($"/dice {outOf}");
+            WriteCommand($"{firstCommand}", 10);
+            WriteCommand($"/dice {outOf}", 1000);
         }
 
         private void EnqueueMessage(string message, XivChatType? chatChannel = null, int minSpacingBeforeInMs = 0, string? targetFullName = null)
